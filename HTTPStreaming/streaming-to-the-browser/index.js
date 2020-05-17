4cae169e581fec375a05eeb9d50d98f4f3ee93ad
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-05-17 15:13:09 +0800

    handled video size

--- HTTPStreaming/streaming-to-the-browser/index.js
+++ HTTPStreaming/streaming-to-the-browser/index.js
@@ -1,9 +1,13 @@
 const { createServer } = require("http");
-const { createReadStream } = require("fs");
+const { stat, createReadStream } = require("fs");
+const { promisify } = require("util");
 const fileName = "../../AdvancedStreams/stream/powder-day.mp4";
+const fileInfo = promisify(stat);
 
-createServer((req, res) => {
+createServer(async (req, res) => {
+  const { size } = await fileInfo(fileName);
   res.writeHead(200, {
+    "Content-Length": size,
     "Content-Type": "video/mp4",
   });
   createReadStream(fileName).pipe(res);
